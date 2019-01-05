3e88676fc8ff2480e42a9799cacb392ca77b7619
Rajiv Shah <rajivshah1@icloud.com>
2019-01-05 14:58:10 -0500

    Add @iota/mam, rn-nodeify, and RN crypto-related packages

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import * as Mam from '@iota/mam';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
